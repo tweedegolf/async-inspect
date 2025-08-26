c2e8cca1f924e949468641406bfede0ee1443a8a
EbbDrop <ebbesteenhoudt@gmail.com>
2025-08-26 18:37:07 +0200

    Some cleanup

--- async_inspect_gdb/src/embassy_inspector/dwarf_parser/future.rs
+++ async_inspect_gdb/src/embassy_inspector/dwarf_parser/future.rs
@@ -6,56 +6,22 @@ use super::{
 };
 
 #[derive(Debug, Clone)]
-pub(crate) struct SelectFutureType {
+pub(crate) struct SelectFuture {
     pub(crate) awaitees: Box<[(u64, Type)]>,
 }
 
-impl SelectFutureType {
+impl SelectFuture {
     fn from_ddbug_select_array(
-        ddbug_type: &ddbug_parser::Type<'_>,
+        ddbug_type: &ddbug_parser::StructType<'_>,
         file_hash: &FileHash<'_>,
-    ) -> Result<Option<Self>> {
-        let TypeKind::Struct(struct_type) = ddbug_type.kind() else {
-            return Ok(None);
-        };
-        // Looks for embassy_futures::select::SelectArray<...>
-        if struct_type
-            .name()
-            .is_none_or(|n| !n.starts_with("SelectArray<"))
-            || struct_type.namespace().and_then(|n| n.name()) != Some("select")
-            || struct_type
-                .namespace()
-                .and_then(|n| n.parent())
-                .and_then(|n| n.name())
-                != Some("embassy_futures")
-        {
-            return Ok(None);
-        }
-
-        let [inner] = struct_type.members() else {
+    ) -> Result<Self> {
+        let [inner] = ddbug_type.members() else {
             return Err("Expected SelectArray to have a single field".into());
         };
-        let s = match inner.ty(file_hash).as_ref().map(|ty| ty.kind()) {
-            Some(TypeKind::Array(array_type)) => {
-                let ty = array_type.element_type(file_hash);
-                let ty = Type::from_maybe_ddbug_type(ty, file_hash);
 
-                let count = array_type
-                    .counts()
-                    .next()
-                    .flatten()
-                    .ok_or("Could not determain the count of the SelectArray")?;
-                let size = array_type
-                    .byte_size(file_hash)
-                    .ok_or("Could not determain the size of the SelectArray")?;
-                let size_of_element = size / count;
-
-                let awaitees = (0..count)
-                    .map(|i| (size_of_element * i, ty.clone()))
-                    .collect();
-
-                Self { awaitees }
-            }
+        let ty = inner.ty(file_hash);
+        let array_type = match ty.as_ref().map(|ty| ty.kind()) {
+            Some(TypeKind::Array(array_type)) => array_type,
             other => {
                 return Err(format!(
                     "Expected SelectArray's inner field to have a array type, not: {other:?}"
@@ -64,33 +30,31 @@ impl SelectFutureType {
             }
         };
 
-        Ok(Some(s))
+        let ty = array_type.element_type(file_hash);
+        let ty = Type::from_maybe_ddbug_type(ty, file_hash);
+
+        let count = array_type
+            .counts()
+            .next()
+            .flatten()
+            .ok_or("Could not determain the count of the SelectArray")?;
+        let size = array_type
+            .byte_size(file_hash)
+            .ok_or("Could not determain the size of the SelectArray")?;
+        let size_of_element = size / count;
+
+        let awaitees = (0..count)
+            .map(|i| (size_of_element * i, ty.clone()))
+            .collect();
+
+        Ok(Self { awaitees })
     }
 
     fn from_ddbug_select_fixed_size(
-        ddbug_type: &ddbug_parser::Type<'_>,
+        ddbug_type: &ddbug_parser::StructType<'_>,
         file_hash: &FileHash<'_>,
-    ) -> Result<Option<Self>> {
-        const NAMES: &[&str; 3] = &["Select<", "Select3<", "Select4<"];
-
-        let TypeKind::Struct(struct_type) = ddbug_type.kind() else {
-            return Ok(None);
-        };
-        // Looks for embassy_futures::select::Select(|3|4)<...>
-        if struct_type
-            .name()
-            .is_none_or(|n| !NAMES.iter().any(|name| n.starts_with(name)))
-            || struct_type.namespace().and_then(|n| n.name()) != Some("select")
-            || struct_type
-                .namespace()
-                .and_then(|n| n.parent())
-                .and_then(|n| n.name())
-                != Some("embassy_futures")
-        {
-            return Ok(None);
-        }
-
-        let awaitees = struct_type
+    ) -> Result<Self> {
+        let awaitees = ddbug_type
             .members()
             .into_iter()
             .map(|member| {
@@ -100,19 +64,43 @@ impl SelectFutureType {
             })
             .collect();
 
-        Ok(Some(Self { awaitees }))
+        Ok(Self { awaitees })
     }
 
     fn from_ddbug_type(
         ddbug_type: &ddbug_parser::Type<'_>,
         file_hash: &FileHash<'_>,
     ) -> Result<Option<Self>> {
-        if let Some(array) = Self::from_ddbug_select_array(ddbug_type, file_hash)? {
-            return Ok(Some(array));
+        let TypeKind::Struct(struct_type) = ddbug_type.kind() else {
+            return Ok(None);
+        };
+
+        // Looks for paths starting embassy_futures::select
+        if struct_type.namespace().and_then(|n| n.name()) != Some("select")
+            || struct_type
+                .namespace()
+                .and_then(|n| n.parent())
+                .and_then(|n| n.name())
+                != Some("embassy_futures")
+        {
+            return Ok(None);
+        }
+        let Some(name) = struct_type.name() else {
+            return Ok(None);
+        };
+
+        if name.starts_with("SelectArray") {
+            let future = Self::from_ddbug_select_array(struct_type, file_hash)?;
+            return Ok(Some(future));
         }
 
-        if let Some(array) = Self::from_ddbug_select_fixed_size(ddbug_type, file_hash)? {
-            return Ok(Some(array));
+        const FIXED_SIZE_NAMES: &[&str; 3] = &["Select<", "Select3<", "Select4<"];
+        if FIXED_SIZE_NAMES
+            .iter()
+            .any(|fixed_size_name| name.starts_with(fixed_size_name))
+        {
+            let future = Self::from_ddbug_select_fixed_size(struct_type, file_hash)?;
+            return Ok(Some(future));
         }
 
         Ok(None)
@@ -122,7 +110,7 @@ impl SelectFutureType {
 #[derive(Debug, Clone)]
 pub(crate) enum FutureTypeKind {
     AsyncFn(AsyncFnType),
-    Select(SelectFutureType),
+    Select(SelectFuture),
 }
 
 #[derive(Debug, Clone)]
@@ -145,7 +133,7 @@ impl FutureType {
             }));
         }
 
-        if let Some(join_future_type) = SelectFutureType::from_ddbug_type(ddbug_type, file_hash)? {
+        if let Some(join_future_type) = SelectFuture::from_ddbug_type(ddbug_type, file_hash)? {
             return Ok(Some(Self {
                 ty,
                 kind: FutureTypeKind::Select(join_future_type),
@@ -158,13 +146,13 @@ impl FutureType {
 
 #[derive(Debug)]
 pub(crate) struct SelectValue {
-    pub(crate) ty: SelectFutureType,
+    pub(crate) ty: SelectFuture,
     pub(crate) awaitees: Box<[FutureValue]>,
 }
 
 impl SelectValue {
     fn new(
-        select_type: &SelectFutureType,
+        select_type: &SelectFuture,
         bytes: &[u8],
         future_types: &std::collections::HashMap<Type, FutureType>,
     ) -> Self {
